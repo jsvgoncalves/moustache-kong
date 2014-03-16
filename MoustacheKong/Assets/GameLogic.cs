30b1afb178dd9cb3feb38c75af69e6eb2285df74
João Gonçalves <jsvgoncalves@gmail.com>
2014-03-16 01:21:55 +0000

    Added GameLogic and PlayerTracker scripts.
    Toggles between cameras pressing C key. Tracking
    still needs to take direction into account.

---
+++ MoustacheKong/Assets/GameLogic.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public Camera Camera2D;
+	public Camera Camera3D;
+
+	// Use this for initialization
+	void Start () {
+		// Defaults 2D camera
+		Camera2D.enabled = true;
+		Camera3D.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/** 
+	 * Physics (and controls) stuff goes here.
+	 **/
+	void FixedUpdate() {
+		if (Input.GetKeyDown(KeyCode.C)) {
+			Camera2D.enabled = !Camera2D.enabled;
+			Camera3D.enabled = !Camera3D.enabled;
+		}
+	}
+
+	/**
+	 * Changes the game to 2D mode.
+	 * - Toggles the camera to ortographic
+	 * - Activates coliders on stairs
+	 **/
+	void changeTo2D() {
+
+	}
+
+
+	/**
+	 * Changes the game to 3D mode.
+	 * - Toggle the camera to perspective
+	 * - Deactivates coliders on stairs
+	 */
+	void changeTo3D() {
+	}
+}
---
+++ MoustacheKong/Assets/PlayerTracker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTracker : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		//TODO: Get the direction from the current platform
+		int dir = 1;
+		pos.x = player.transform.position.x + 5 * dir;
+		pos.y = player.transform.position.y + 2;
+		pos.z = player.transform.position.z;
+
+		transform.position = pos;
+	}
+}
