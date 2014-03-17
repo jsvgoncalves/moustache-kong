46cdfa4749532e0fece5d521d84696ef422cf534
João Gonçalves <jsvgoncalves@gmail.com>
2014-03-17 00:44:30 +0000

    Camera now changes direction based on platform.
    Raycasting and getting the tag of the object ("fwd" or
    "bck").

--- MoustacheKong/Assets/scripts/PlayerTracker.cs
+++ MoustacheKong/Assets/scripts/PlayerTracker.cs
@@ -5,6 +5,8 @@ public class PlayerTracker : MonoBehaviour {
 
 	public GameObject player;
 
+	private int dir = 1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,13 +14,33 @@ public class PlayerTracker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+		// Get the current platform
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position + Vector3.up, Vector3.down, out hit, 10)) {
+			if (hit.collider.gameObject.tag.Equals ("bck")) {
+				dir = -1;
+			} else if(hit.collider.gameObject.tag.Equals ("fwd")) {
+				dir = 1;
+			}
+			print (hit.collider.gameObject.tag);
+		}
+
 		Vector3 pos = transform.position;
 		//TODO: Get the direction from the current platform
-		int dir = 1;
-		pos.x = player.transform.position.x + 5 * dir;
+
+		pos.x = player.transform.position.x - 6 * dir;
 		pos.y = player.transform.position.y + 2;
 		pos.z = player.transform.position.z;
 
+		Quaternion rot = transform.rotation;
+		rot = Quaternion.Euler(0, dir * 90,0);
+
 		transform.position = pos;
+		transform.rotation = rot;
+
+
+		
 	}
 }
