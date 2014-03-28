5c6a0a2e7f7b1ca5594f3a1723bbabc89d023e29
Filipe Rodrigues <subrodrigues@gmail.com>
2014-03-28 16:19:27 +0000

    last version

--- MoustacheKong/Assets/scripts/GameLogic.cs
+++ MoustacheKong/Assets/scripts/GameLogic.cs
@@ -67,7 +67,6 @@ public class GameLogic : MonoBehaviour {
 			} else {
 				changeTo2D();
 			}
-			
 		}
 	}
 	
@@ -83,7 +82,6 @@ public class GameLogic : MonoBehaviour {
 		//GameObject.FindGameObjectWithTag ("Player").GetComponent(CharacterController).enabled = false;
 		gameObject.GetComponent<CharacterController> ().enabled = false;
 	}
-	
 	
 	/**
 	 * Changes the game to 3D mode.
