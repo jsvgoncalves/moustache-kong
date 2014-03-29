05c40e054e623cfbbb4e6b9620381c2794f3f830
João Gonçalves <jsvgoncalves@gmail.com>
2014-03-29 17:06:29 +0000

    Fixed barrel movement.

--- MoustacheKong/Assets/scripts/BarrelScript.cs
+++ MoustacheKong/Assets/scripts/BarrelScript.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 
 public class BarrelScript : MonoBehaviour {
-
-
+	
+	
 	Vector3 velocity = Vector3.zero;
-
+	
 	public Vector3 gravity;
 	public Vector3 gravityX;
 	public Vector3 jumpVelocity;
 	public float maxSpeed = 5f;
-
+	
 	// Used to calculate the movement direction
 	private int dir = 1;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		// Choose a random lane to start?
@@ -24,10 +24,10 @@ public class BarrelScript : MonoBehaviour {
 	void Update () {
 		// stuff maybe?
 	}
-
+	
 	void FixedUpdate() {
 		velocity.y = 0;
-
+		
 		// Get the current platform
 		RaycastHit hit;
 		// The direction is actually the opposite of the platform tag. (Goes against the player)
@@ -37,19 +37,20 @@ public class BarrelScript : MonoBehaviour {
 			} else if(hit.collider.gameObject.tag.Equals ("fwd") && hit.distance < 2) {
 				dir = -1;
 			}
-
-
+			
+			
 			if(hit.distance > 0.5 ) {
 				velocity.y = gravity.y;
 			}
-//			Debug.Log(hit.distance);
-//			Debug.Log(hit.collider.gameObject.tag);
+			
+			//			Debug.Log(hit.distance);
+			//			Debug.Log(hit.collider.gameObject.tag);
 		}
-
+		
 		velocity.x = dir * gravityX.x;
 		velocity.z = 0;
-
-//		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+		
+		//		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
 		
 		transform.position += velocity * Time.deltaTime;
 	}
