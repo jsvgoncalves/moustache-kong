88a43ecc1c2cd32e7481bae9d55a95a0a0ca20d1
EvaMarques <evamasmarques@gmail.com>
2014-04-07 15:02:01 +0100

    Fix bug related to platform dependent build

--- MoustacheKong/Assets/scripts/GizmoShortcuts.cs
+++ MoustacheKong/Assets/scripts/GizmoShortcuts.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR 
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -70,3 +71,4 @@ public class GizmoShortcuts {
        return activeSceneView;
     }
 }
+#endif
